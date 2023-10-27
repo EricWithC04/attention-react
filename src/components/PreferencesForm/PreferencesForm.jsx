51af2a93cfbd17f222907d2cd560a34b089e66cc
EricWithC04 <negativegame117@gmail.com>
2023-10-27 11:33:17 -0300

    Initial Commit

---
+++ src/components/PreferencesForm/PreferencesForm.jsx
@@ -0,0 +1,223 @@
+import React, { useState, useMemo } from 'react'
+import logo from "../../assets/logo-2.png"
+import "./PreferencesForm.css"
+
+const PreferencesForm = () => {
+
+    const [preferences, setPreferences] = useState({
+        subject: "",
+        time_day: "",
+        people: "",
+        contact_type: "",
+        contact: ""
+    })
+
+    const [errorsActive, setErrorsActive] = useState(false)
+
+    const handleChange = (e) => {
+
+        console.log("changed")
+        setPreferences((pref) => ({
+            ...pref,
+            [e.target.name]: e.target.value
+        }))
+
+        console.log(preferences);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (!errorsActive) setErrorsActive(true)
+
+        console.log(errors)
+
+        if (!Object.keys(errors).length) {
+            const token = localStorage.getItem("token")
+
+            fetch("http://localhost:3000/api/users/preferences", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "authorization": token
+                },
+                body: JSON.stringify(preferences),
+            })
+                .then(res => res.json())
+                .then((res) => console.log(res))
+        }
+
+    }
+
+    const errors = useMemo(() => {
+        let formErrors = {}
+        if (!errorsActive) return {};
+        if (!preferences.subject.length) formErrors.subject = "Debes elegír una materia!"
+        if (!preferences.time_day.length) formErrors.time_day = "Debes elegír un momento del día!"
+        if (!preferences.people.length) formErrors.people = "Debes elegír que estas buscando!"
+        if (!preferences.contact_type.length) formErrors.contact_type = "Debes elegír el tipo de contacto!"
+        if (!preferences.contact.length) formErrors.contact = "Debes ingresar tus datos de contacto!"
+        if (preferences.contact_type === "Numero Telefónico" && preferences.contact.length < 11) {
+            formErrors.contact = "Debes agregar un numero telefonico valido"
+        }
+        if (preferences.contact_type === "Discord" && !preferences.contact.length) {
+            formErrors.contact = "Debes agregar tu usuario de Discord!"
+        }
+        if (preferences.contact_type === "Slack" && !preferences.contact.length) {
+            formErrors.contact = "Debes agregar tu usuario de Slack!"
+        }
+
+        return formErrors;
+    }, [preferences, errorsActive])
+
+    const materias = [
+        "Matemáticas",
+        "Lenguaje",
+        "Física",
+        "Química",
+        "Economía",
+        "Geografía",
+        "Estudios Sociales",
+        "Informática"
+    ]
+
+    return (
+        <form
+            action=""
+            className="d-flex flex-column border border-3 rounded-3 w-50 mt-5 mb-4 text-color bg-grey"
+            id="formulario"
+            onChange={handleChange}
+            onSubmit={handleSubmit}
+        >
+            <h1 class="fs-1 m-3 purple align-self-center">Preferencias de Usuario</h1>
+            <div className='d-flex justify-content-center w-100'>
+
+                <div className="m-3 d-flex flex-column w-50" name="materias">
+                    <label className="form-label">¿Qué se le dificulta?</label>
+                    <div>
+                        <input type="radio" className="form-check-input" name="subject" value="" />
+                        <span> Ninguna</span>
+                    </div>
+                    {
+                        materias.map(subject => {
+                            return (
+                                <div>
+                                    <input type="radio" className="form-check-input" name="subject" value={subject} />
+                                    <span> {subject}</span>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div>
+
+                    <div>
+                        {
+                            errors.hasOwnProperty("subject") && errors.subject.length ?
+                                (<p className='bg-danger text-white ms-2 me-2 rounded p-1'>{errors.subject}</p>) : null
+                        }
+                    </div>
+                    <div className="m-3">
+                        <label className="form-label">
+                            ¿En qué momento del día puede estudiar?
+                        </label>
+                        <select name="time_day" id="" className="form-select">
+                            <option value="">Seleccione una opción</option>
+                            <option>Día</option>
+                            <option>Tarde</option>
+                            <option>Noche</option>
+                        </select>
+                    </div>
+                    <div>
+                        {
+                            errors.hasOwnProperty("time_day") && errors.time_day.length ?
+                                (<p className='bg-danger text-white ms-2 me-2 rounded p-1'>{errors.time_day}</p>) : null
+                        }
+                    </div>
+                    <div className="m-3">
+                        <label className="form-label"> ¿Qué le gustaría encontrar? </label>
+                        <select name="people" className="form-select">
+                            <option value="">Seleccione una opción</option>
+                            <option>alguien que me acompañe al estudiar</option>
+                            <option>despejar dudas</option>
+                            <option>tutoría</option>
+                        </select>
+                    </div>
+                    <div>
+                        {
+                            errors.hasOwnProperty("people") && errors.people.length ?
+                                (<p className='bg-danger text-white ms-2 me-2 rounded p-1'>{errors.people}</p>) : null
+                        }
+                    </div>
+                    <div className="m-3 d-flex flex-column" id="contacts">
+                        <label className="form-label"> ¿Qué forma de contacto utiliza? </label>
+                        <div>
+                            <input
+                                type="radio"
+                                className="form-check-input"
+                                name="contact_type"
+                                id="cel"
+                                value="Numero Telefónico"
+                            />
+                            <span> Numero Telefónico</span>
+                        </div>
+                        <div>
+                            <input
+                                type="radio"
+                                className="form-check-input"
+                                name="contact_type"
+                                id="discord"
+                                value="Discord"
+                            />
+                            <span> Discord</span>
+                        </div>
+                        <div>
+                            <input
+                                type="radio"
+                                className="form-check-input"
+                                name="contact_type"
+                                id="slack"
+                                value="Slack"
+                            />
+                            <span> Slack</span>
+                        </div>
+                        <div id="contact-input">
+                            {
+                                preferences.contact_type === "Numero Telefónico" ? (
+                                    <input
+                                        type="number"
+                                        placeholder='000-0000-0000'
+                                        className='w-100 form-control'
+                                        name='contact'
+                                    />
+                                ) :
+                                    preferences.contact_type === "Discord" || preferences.contact_type === "Slack" ? (
+                                        <input
+                                            placeholder={`@usuario de ${preferences.contact_type}`}
+                                            className='w-100 form-control'
+                                            name='contact'
+                                        />
+                                    ) : null
+                            }
+                        </div>
+                    </div>
+                    <div>
+                        {
+                            errors.hasOwnProperty("contact") && errors.contact.length ?
+                                (<p className='bg-danger text-white ms-2 me-2 rounded p-1'>{errors.contact}</p>) : null
+                        }
+                    </div>
+                </div>
+            </div>
+            <input
+                type="submit"
+                className="btn bg-purple text-white m-3"
+                value="Submit"
+            />
+            <div class="logo-with-text w-100 border-3 border-top mt-4 d-flex justify-content-center gap-3 align-items-center">
+                <img src={logo} class="my-auto" alt="Logo Attention" />
+            </div>
+        </form>
+    )
+}
+
+export default PreferencesForm
---
+++ src/components/UserProfile/UserProfile.jsx
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from 'react'
+import userImg from "../../assets/user.jpg"
+import "./UserProfile.css"
+
+const UserProfile = () => {
+
+    const [userData, setUserData] = useState({})
+
+    useEffect(() => {
+        const token = localStorage.getItem("token")
+
+        fetch("http://localhost:4001/user/info", {
+            method: "GET",
+            headers: {
+                "content-type": "application/json",
+                "authorization": token
+            }
+        })
+            .then(res => res.json())
+            .then(res => setUserData(res))
+        // setUserData({
+        //     id: 1,
+        //     name: "EricWithC04",
+        //     email: "negativegame117@gmail.com",
+        //     password: "$2b$07$AGTLb6SWVqt52CsiTkoAzuS3gY.rM7AOJEFaC5BLnV68Dc676WCP2",
+        //     description: "Estudiante de Desarrollo de Software",
+        //     ocupation: null,
+        //     problem: null,
+        //     createdAt: "2023-10-18T14:53:08.000Z",
+        //     updatedAt: "2023-10-18T14:53:08.000Z",
+        //     deletedAt: null,
+        //     subscriptionPayload: null,
+        //     preferences: [
+        //         {
+        //             time_day: "Día",
+        //             subject: "Economía",
+        //             contact: "",
+        //             people: "tutoría",
+        //             contact_type: "534534535"
+        //         }
+        //     ]
+        // })
+    }, [])
+
+    return (
+        <div className="container">
+            {
+                userData.hasOwnProperty("name") ? (
+                    <div className="d-flex flex-column">
+                        <div className="d-flex align-items-center w-100">
+                            <img src={userImg} className="profile rounded-circle mt-4 mb-4 ms-5" alt="foto" />
+                            <div className="ms-5 mt-5 text-color">
+                                <h2 className="fw-bold purple mb-3" id="name">{userData.name}</h2>
+                                <h4 id="description">{userData.description}</h4>
+                                <h4 className="lead lead-size" id="email">{userData.email}</h4>
+                            </div>
+                            <button className="btn btn-size bg-purple text-white align-self-end ms-auto" id="btn-preferences">Preferencias</button>
+                        </div>
+                        <div className="text-color border p-4 border-3 rounded-2 bg-grey z-position">
+                            <legend>Se le dificulta: </legend>
+                            <p id="subject">{userData.preferences[0].subject}</p>
+                            <hr />
+                            <legend>Puede estudiar:</legend>
+                            <p id="time_day">{userData.preferences[0].time_day}</p>
+                            <hr />
+                            <legend>Busca</legend>
+                            <p id="people">{userData.preferences[0].people}</p>
+                            <hr />
+                            <legend>Puede contactarse a traves de:</legend>
+                            <p id="contact">{`${userData.preferences[0].contact} : ${userData.preferences[0].contact_type}`}</p>
+                        </div>
+                    </div>
+                ) : <div>Cargando...</div>
+            }
+
+        </div>
+    )
+}
+
+export default UserProfile
