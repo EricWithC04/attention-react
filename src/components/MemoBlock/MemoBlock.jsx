006b22c53d6ad6af0b5db54708e18aa2159404be
EricWithC04 <negativegame117@gmail.com>
2023-12-02 10:50:30 -0300

    feat: attention logo in cards of memoTest

--- src/components/MemoBlock/MemoBlock.jsx
+++ src/components/MemoBlock/MemoBlock.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import logo from "../../assets/logo-1.png"
 import style from './MemoBlock.module.css'
 
 const MemoBlock = ({ handleMemoClick, animating, memoBlock }) => {
     return (
         <div className={style.memoBlock} onClick={() => (!animating && !memoBlock.flipped) ? handleMemoClick(memoBlock) : null}>
             <div className={`${style.memoBlockInner} ${memoBlock.flipped ? style.memoBlockFlipped : ''}`}>
-                <div className={style.memoBlockFront}></div>
+                <div className={style.memoBlockFront}>
+                    <img src={logo} />
+                </div>
                 <div className={style.memoBlockBack}>
                     {memoBlock.emoji}
                 </div>
